3b132cc34ac7340bf976c6a87397d104966a2760
Allan Young <ayounggtr@gmail.com>
2023-12-08 12:27:26 -0500

    Javascript Round 1

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & Exercise 2
 
-// Exercise 2
+const firstname = "Chuck";
+let lastname = "Norris";
+var age = 83;
 
 // Exercise 3
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale  = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+// Exercise 5
+let title = "Name Of The Wind";
+let keyword = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+
